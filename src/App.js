25afd3997a18721086c86ae030a002caf5e848cb
weiwenl <weiwenl.justin@gmail.com>
2018-11-24 16:45:56 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -7,18 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Marketplace</h1>
+          <h2>Get board games cheap, <br/>compare prices at different stores.</h2>
         </header>
       </div>
     );
