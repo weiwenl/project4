2bfa69162344e980f365a799fe0bac6c01f86201
weiwenl <weiwenl.justin@gmail.com>
2018-12-03 10:57:21 +0800

    Got games id, use it to query for prices and links

--- src/components/pages/ProductPrices.js
+++ src/components/pages/ProductPrices.js
@@ -1,12 +1,75 @@
 // import libary components
-import React from "react";
+import React, {Fragment, Component} from 'react';
+import {Card} from 'semantic-ui-react'
 
-const ProductPrices = ({match}) => {
+class ProductPrices extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: [],
+      prices: []
+    };
+  }
 
-    return(
-          <h1>This is {match.params.id}</h1>
-        );
-};
+  componentDidMount() {
+    console.log(this.props.location.pathname);
+    let pathname = this.props.location.pathname;
+    let array = pathname.split('/');
+    console.log(array[3]);
 
+    const URL = "https://beta.5colorcombo.com/api/search?ids=" + encodeURI(array[3]);
+    console.log(URL);
+    fetch(URL).then( (response) => {
+      response.json().then( (data) => {
+        this.setState({
+            item: data.games
+        })
+      })
+    })
+    const URL2 = "https://www.5colorcombo.com/api/game/prices?game-id=" + encodeURI(array[3]);
+    console.log(URL2);
+    fetch(URL2).then( (response) => {
+      response.json().then( (data) => {
+        this.setState({
+            prices: data.prices
+        })
+      })
+    })
+
+  }
+
+  render() {
+    let searchItem = this.state.item.map((item, index) => {
+      return(
+        <Card
+          key={index}
+          image={item.image_url}
+          header={item.name}
+        />
+          );
+    });
+
+    let searchPrices = this.state.prices.map((price, index) => {
+      return(
+            <Card
+              key={index} // Unique key identifier for React
+              description={price.price_text}
+              header={price.store_name}
+              meta={price.country}
+              href={price.url}
+            />
+          );
+    });
+
+    return (
+      <Fragment>
+        {searchItem}
+        <Card.Group>
+              {searchPrices}
+        </Card.Group>
+      </Fragment>
+    );
+  }
+}
 
 export default ProductPrices
